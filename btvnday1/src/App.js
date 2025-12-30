a431ddbc81f8dc24fe8bea587d1ceb29073f75bb
Tranloc06 <tranhuuloc2602@gmail.com>
2025-12-30 18:59:55 +0700

    ok

---
+++ btvnday1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Tran huu loc</h1>
+      <h4>Nam sinh: 2006</h4>
+      <h4>Nghe nghiep:Sinh vien</h4>
+     
+
+    </div>
+  )
+}
+
+export default App
